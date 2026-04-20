26bb3bf775e583ad18b01e73d31d5ab9cc2e1703
anushakarla123 <anusha.karla777@gmail.com>
2026-04-20 14:03:24 +0530

    Create postgres_setup.sql

---
+++ postgres_setup.sql
@@ -0,0 +1,8 @@
+CREATE TABLE students (
+id SERIAL PRIMARY KEY,
+name VARCHAR(50),
+age INT
+);
+
+INSERT INTO students (name, age) VALUES ('Anu', 20);
+INSERT INTO students (name, age) VALUES ('Ravi', 21);
